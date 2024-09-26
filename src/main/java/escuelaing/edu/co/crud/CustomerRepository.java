a907f4dacaee67939638b3e3dbed6101eec54940
leoncico <leoncico895@hotmail.com>
2024-09-26 14:49:52 -0500

    Taller de clase terminado

---
+++ src/main/java/escuelaing/edu/co/crud/CustomerRepository.java
@@ -0,0 +1,11 @@
+package escuelaing.edu.co.crud;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+  List<Customer> findByLastName(String lastName);
+
+  Customer findById(long id);
+}
